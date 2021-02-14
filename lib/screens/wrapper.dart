58457e3dbbe90e753a1bb5e68a474ecf8be67486
isah <princeisah15@gmail.com>
2021-02-13 23:25:14 -0500

    firebase

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:plastiex/screens/authentication/signin.dart';
 import 'package:plastiex/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Home();
+          } else {
+            return LoginScreen();
+          }
+        });
   }
 }
